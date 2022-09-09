6bce2b9878e4b7692b7d87208cb171d681cb7de7
MaxxyFred304 <maxfred304@gmail.com>
2022-09-08 19:02:11 -0700

    script that generates the assembly code

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 /**
-* main - print the string in the put function
-* 
+* main - print the string in the put function 
+*
 * Description: using the main function
-* this program prints "Programming is like building a multilingual puzzle
+* this program prnts "Programming is like building a multilingual puzzle
 * Return: 0
 */
 int main(void)
@@ -12,13 +12,53 @@ int main(void)
 	char c;
 	int i;
 	long li;
-	long long lli;
+	long long li;
 	float f;
 
-	printf("Size of char: %ld byte(s)\n", sizeof(c));
-	printf("Size of an int: %ld byte(s)\n", sizeof(i));
-	printf("Size of a long int: %ld byte(s)\n", sizeof(li));
-	printf('Size oa long long int: %ld bytes(s)\n", sizeof(lli));
-	printf("Size of a float: %ld byte(s)\n", sizeof(f));
-	return (0);
-}
+	printf("Size of a char: %ld byte(s)\n", sizeof(c))''''''''''''''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
