985905d5b88588b6b4db2f48599ab91e01d852af
MaxxyFred304 <maxfred304@gmail.com>
2022-09-10 04:54:46 -0700

    Print alphabet apart from e and q

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,17 +1,21 @@
 #include <stdio.h>
 /**
- * main -Prints the alphabet in lowercase, except e and q
+ * main - print if the number is positive, zero, or negative
  *
- * Return: 0;
+ * Description: using the main function
+ * this program prints "Programming is postive, zero, or negative
+ * Return: 0
  */
 int main(void)
 {
-	char ch;
-	for (ch = 'a' ; ch <= 'z' ; ch++)
-	{
-		if (ch != 'e' && ch != 'q')
-	           putchar(ch);
-	}
-	putchar('\n');
-	return (0);
+char ch;
+for (ch = 'a' ; ch <= 'z' ; ch++)
+{
+if (ch != 'e' && ch != 'q')
+{
+	putchar(ch);
+}
+}
+putchar('\n');
+return (0);
 }
