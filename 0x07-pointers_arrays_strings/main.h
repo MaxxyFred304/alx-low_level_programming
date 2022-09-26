1d3c4bd8ad2c72dd62ba5aeccdaaac340700b33c
MaxxyFred304 <maxfred304@gmail.com>
2022-09-26 12:31:02 -0700

    function that fills memory with a constant byte.

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,7 +1,12 @@
 #ifndef MAIN_H
 #define MAIN_H
-#include <stdio.h>
 
+/**
+* void prototypes()
+* int prototypes()
+*/
+
+int _putchar(char);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
