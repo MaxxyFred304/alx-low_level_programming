7b3bb90f6d5c82f96edcb595d71f1934663ee073
MaxxyFred304 <maxfred304@gmail.com>
2022-09-28 14:03:29 -0700

    natural square root of a number.

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,18 +1,16 @@
 #include "main.h"
 
-int actual_prime(int n, int i);
-
 /**
  * is_prime_number - says if an integer is a prime number or not
  * @n: number to evaluate
  *
  * Return: 1 if n is a prime number, 0 if not
  */
-int is_prime_number(int n)
+int _sqrt_recursion(int n)
 {
-	if (n <= 1)
-		return (0);
-	return (actual_prime(n, n - 1));
+	if (n == 1 || n == 0)
+		return (n);
+	return (_sqrt(0, n));
 }
 
 /**
@@ -22,11 +20,11 @@ int is_prime_number(int n)
  *
  * Return: 1 if n is prime, 0 if not
  */
-int actual_prime(int n, int i)
+int _sqrt(int n, int x)
 {
-	if (i == 1)
-		return (1);
-	if (n % i == 0 && i > 0)
-		return (0);
-	return (actual_prime(n, i - 1));
+	if (n > x / 2)
+		return (-1);
+	else if (n * n == x)
+		return (n);
+	return (_sqrt(n + 1, x));
 }
